7c87423e3fcdf241b8b97ec5dcaee06a36c02c68
student12 <student12@pcoip.ki.agh.edu.pl>
2022-05-28 12:07:05 +0200

    logOut

---
+++ src/AuthPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Witaj {{ username }}</h2>
+        <a @click="logOut()">Wyloguj</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['username'],
+        methods: {
+            logOut() {
+                this.$emit('logOut', this.email);
+            }
+        },
+    }
+</script>
