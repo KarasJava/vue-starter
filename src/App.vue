d37d15a3fd1cbe3814902205d237c679725e7fa8
student12 <student12@pcoip.ki.agh.edu.pl>
2022-05-28 10:13:00 +0200

    adding conditional to input field

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
     <div>
         <h1>Twój e-mail to {{ email }}</h1>
 
-        <div v-if="email.length < 10">Ale masz krótki adres!</div>
+        <div v-if="email.length < 10" class="blue">Ale masz krótki adres!</div>
         <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-        <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+        <div v-else class="red">Twój adres e-mail jest stanowczo za długi.</div>
 
         <input type="email" v-model="email">
     </div>
@@ -19,3 +19,12 @@
         }
     }
 </script>
+
+<style >
+.red{
+  color:red;
+}
+.blue{
+  color:blue;
+}
+</style>
